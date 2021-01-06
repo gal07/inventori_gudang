67af0136a5e071dec0aa5f5811eb524766eecc5d
galih <galihkur29@gmail.com>
2021-01-06 12:01:50 +0700

    update db

--- db/inventori_gudang.sql
+++ db/inventori_gudang.sql
@@ -1,173 +1,221 @@
-/*
-SQLyog Community v13.1.1 (64 bit)
-MySQL - 10.1.30-MariaDB : Database - inventori_gudang
-*********************************************************************
-*/
+-- MySQL dump 10.13  Distrib 8.0.22, for Linux (x86_64)
+--
+-- Host: localhost    Database: inventori_gudang
+-- ------------------------------------------------------
+-- Server version	8.0.22-0ubuntu0.20.04.3
 
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`inventori_gudang` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `inventori_gudang`;
-
-/*Table structure for table `barang` */
+--
+-- Table structure for table `barang`
+--
 
 DROP TABLE IF EXISTS `barang`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `barang` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nama_barang` varchar(100) DEFAULT NULL,
   `jenis` varchar(100) DEFAULT NULL,
-  `stock` int(11) DEFAULT NULL,
+  `stock` int DEFAULT NULL,
   `harga` decimal(18,0) DEFAULT NULL,
   `picture` varchar(100) DEFAULT NULL,
-  `active` int(1) DEFAULT NULL,
+  `active` int DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `barang` */
+--
+-- Dumping data for table `barang`
+--
 
-insert  into `barang`(`id`,`nama_barang`,`jenis`,`stock`,`harga`,`picture`,`active`) values 
-(3,'Pensil 5B Aasus','Alat Perkantoran',48,15000,'20190922123956.jpg',1),
-(8,'Pensil 5B Aasus','Alat Elektronik',89,50000,'20190923112313.jpg',1),
-(9,'Asus ROG 2000','Alat Elektronik',52,2000000,'20200104072550.jpg',1),
-(10,'Firebase','Alat Elektronik',55,450000,'20200921053433.jpg',1),
-(11,'Mouse Gaming HP M160','Alat Elektronik',5,160000,'20201226023057.jpg',1),
-(12,'Mouse Gaming AULA DPI 2100','Alat Elektronik',90,70000,'20201226052816.jpg',1),
-(13,'Mouse Gaming Red Dragon M920','Alat Elektronik',55,100000,'20201226055752.jpg',1);
+LOCK TABLES `barang` WRITE;
+/*!40000 ALTER TABLE `barang` DISABLE KEYS */;
+INSERT INTO `barang` VALUES (8,'Pensil 5B Aasus','Alat Elektronik',89,50000,'20190923112313.jpg',1),(9,'Asus ROG 2000','Alat Elektronik',52,2000000,'20200104072550.jpg',1),(10,'Firebase','Alat Elektronik',55,450000,'20200921053433.jpg',1),(11,'Mouse Gaming HP M160','Alat Elektronik',5,160000,'20201226023057.jpg',1),(12,'Mouse Gaming AULA DPI 2100','Alat Elektronik',90,70000,'20201226052816.jpg',1),(13,'Mouse Gaming Red Dragon M920','Alat Elektronik',55,100000,'20201226055752.jpg',1);
+/*!40000 ALTER TABLE `barang` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `barang_out` */
+--
+-- Table structure for table `barang_out`
+--
 
 DROP TABLE IF EXISTS `barang_out`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `barang_out` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
-  `id_barang` int(5) DEFAULT NULL,
-  `qty` int(5) DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_barang` int DEFAULT NULL,
+  `qty` int DEFAULT NULL,
   `own_by` varchar(25) DEFAULT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `barang_out` */
+--
+-- Dumping data for table `barang_out`
+--
 
-/*Table structure for table `gudang` */
+LOCK TABLES `barang_out` WRITE;
+/*!40000 ALTER TABLE `barang_out` DISABLE KEYS */;
+/*!40000 ALTER TABLE `barang_out` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gudang`
+--
 
 DROP TABLE IF EXISTS `gudang`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `gudang` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nama` varchar(100) NOT NULL,
+  `status` int NOT NULL,
+  `softdelete` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `gudang` */
+--
+-- Dumping data for table `gudang`
+--
 
-insert  into `gudang`(`id`,`nama`) values 
-(1,'Gudang Jakarta'),
-(2,'Gudang Bekasi');
+LOCK TABLES `gudang` WRITE;
+/*!40000 ALTER TABLE `gudang` DISABLE KEYS */;
+INSERT INTO `gudang` VALUES (1,'Gudang Jakarta',1,0),(2,'Gudang Bekasi',1,0),(3,'Gudang depok',1,1),(6,'Gudang Depok',1,0);
+/*!40000 ALTER TABLE `gudang` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `inven_gudang` */
+--
+-- Table structure for table `inven_gudang`
+--
 
 DROP TABLE IF EXISTS `inven_gudang`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `inven_gudang` (
-  `id` bigint(10) NOT NULL AUTO_INCREMENT,
-  `id_gudang` int(5) NOT NULL,
-  `id_barang` int(10) NOT NULL,
-  `qty` int(6) NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id_gudang` int NOT NULL,
+  `id_barang` int NOT NULL,
+  `qty` int NOT NULL,
+  `status` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `gudangConstraint` (`id_gudang`),
   KEY `barangConstraint` (`id_barang`),
   CONSTRAINT `barangConstraint` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `gudangConstraint` FOREIGN KEY (`id_gudang`) REFERENCES `gudang` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  CONSTRAINT `gudangConstraint` FOREIGN KEY (`id_gudang`) REFERENCES `gudang` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `inven_gudang` */
+--
+-- Dumping data for table `inven_gudang`
+--
 
-insert  into `inven_gudang`(`id`,`id_gudang`,`id_barang`,`qty`) values 
-(1,1,3,20),
-(2,1,12,63);
+LOCK TABLES `inven_gudang` WRITE;
+/*!40000 ALTER TABLE `inven_gudang` DISABLE KEYS */;
+INSERT INTO `inven_gudang` VALUES (2,1,12,63,0),(5,2,13,56,0);
+/*!40000 ALTER TABLE `inven_gudang` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `jenis_report` */
+--
+-- Table structure for table `jenis_report`
+--
 
 DROP TABLE IF EXISTS `jenis_report`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `jenis_report` (
-  `id` int(5) NOT NULL,
+  `id` int NOT NULL,
   `name` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `jenis_report` */
+--
+-- Dumping data for table `jenis_report`
+--
 
-insert  into `jenis_report`(`id`,`name`) values 
-(1,'Barang Masuk'),
-(2,'Barang Keluar');
+LOCK TABLES `jenis_report` WRITE;
+/*!40000 ALTER TABLE `jenis_report` DISABLE KEYS */;
+INSERT INTO `jenis_report` VALUES (1,'Barang Masuk'),(2,'Barang Keluar');
+/*!40000 ALTER TABLE `jenis_report` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `report` */
+--
+-- Table structure for table `report`
+--
 
 DROP TABLE IF EXISTS `report`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `report` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
-  `id_barang` int(6) DEFAULT NULL,
-  `quantity` int(6) DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_barang` int DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
   `action_by` varchar(100) DEFAULT NULL,
-  `jenis_report` int(1) DEFAULT NULL,
+  `jenis_report` int DEFAULT NULL,
   `waktu` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `report` */
+--
+-- Dumping data for table `report`
+--
 
-insert  into `report`(`id`,`id_barang`,`quantity`,`action_by`,`jenis_report`,`waktu`) values 
-(1,9,50,'galih',1,'2020-01-04'),
-(2,9,2,'galih',2,'2020-01-04'),
-(3,10,5,'galih',1,'2020-09-21'),
-(4,10,50,'galih',2,'2020-09-21'),
-(5,11,20,'gudang1',1,'2020-12-26'),
-(6,11,45,'gudang1',2,'2020-12-26'),
-(7,12,100,'gudang1',1,'2020-12-26'),
-(8,13,100,'gudang1',1,'2020-12-26'),
-(9,13,25,'gudang1',2,'2020-12-26'),
-(10,13,25,'gudang1',2,'2020-12-26'),
-(11,13,12,'gudang1',1,'2020-12-26'),
-(12,13,7,'gudang1',2,'2020-12-26'),
-(13,12,10,'gudang1',2,'2020-12-26');
+LOCK TABLES `report` WRITE;
+/*!40000 ALTER TABLE `report` DISABLE KEYS */;
+/*!40000 ALTER TABLE `report` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `users` */
+--
+-- Table structure for table `users`
+--
 
 DROP TABLE IF EXISTS `users`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `telepon` varchar(13) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `role` int(1) NOT NULL,
-  `gudang` int(5) DEFAULT NULL,
-  `status` int(1) NOT NULL,
+  `role` int NOT NULL,
+  `gudang` int DEFAULT NULL,
+  `status` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDPARENT` (`gudang`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `users` */
+--
+-- Dumping data for table `users`
+--
 
-insert  into `users`(`id`,`username`,`email`,`telepon`,`password`,`role`,`gudang`,`status`) values 
-(1,'galih','galih@mail.com','0991288384','123456',1,0,1),
-(2,'gudang1','gg@mail.com','08823774823','123456',2,0,1),
-(3,'admin1','ASVA@gmail.com','2312312','678678678',2,0,1),
-(4,'nm1','nn@nn.com','08891277388','12345',2,0,1);
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'galih','galih@mail.com','0991288384','123456',1,0,1),(7,'amirudin','amirudi@gmail.com','09887882882','123456',2,1,1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-01-06 11:57:11
